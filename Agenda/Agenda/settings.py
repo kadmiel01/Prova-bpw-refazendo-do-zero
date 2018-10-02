a4dfc7716cb3081f76070d6da2c3c866b694130a
kadmiel01 <42592155+kadmiel01@users.noreply.github.com>
2018-10-02 12:26:39 -0300

    Fazendo o blog funcionar part-1

--- Agenda/Agenda/settings.py
+++ Agenda/Agenda/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'events',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
-    'events',
+    
 ]
 
 MIDDLEWARE = [
